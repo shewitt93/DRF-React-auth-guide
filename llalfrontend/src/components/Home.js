39df15a324e7e75a8c6b6bffe38912d5781e2930
peyman simon hewitt <shewitt@live.co.uk>
2020-10-13 16:17:10 +0100

    first commit

---
+++ llalfrontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h4>What would you like to do today?</h4>
+      </div>
+    );
+  }
+}
+
+export default Home;
